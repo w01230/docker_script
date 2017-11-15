42f9e0273a69dd3cbe4ff1d216d4c191f4dd18c3
01230 <4585006@GMAIL.COM>
2017-11-15 14:32:54 +0800

    Update ipsec.sh

--- ipsec.sh
+++ ipsec.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 docker run --name ipsec \
-	-e "VPN_IPSEC_PSK=0x01230" \
-	-e "VPN_USER=w01230" \
-	-e "VPN_PASSWORD=0x0000" \
+	-e "VPN_IPSEC_PSK=xxxxxx" \
+	-e "VPN_USER=xxxxxx" \
+	-e "VPN_PASSWORD=xxxxxx" \
 	--restart=always \
 	-p 500:500/udp \
 	-p 4500:4500/udp \

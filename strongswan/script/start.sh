ca812f9e85d3acde7b7e5f73f133a93ecfcf7727
01230 <4585006@GMAIL.COM>
2017-11-17 15:45:02 +0800

    Update start.sh

--- strongswan/script/start.sh
+++ strongswan/script/start.sh
@@ -4,9 +4,8 @@ if [ "$1" == "init" ]; then
 	./root/init.sh
 else
 	echo "Starting VPN server"
-	touch /var/log/charon.log
+	rm -f /var/run/starter.charon.pid
 	iptables -t nat -A POSTROUTING -s 10.0.2.0/24 -j MASQUERADE
 	sysctl -w net.ipv4.ip_forward=1
 	/usr/sbin/ipsec start --nofork
-	tail -f /var/log/charon.log
 fi

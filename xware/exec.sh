5fdfe5ee977f19406c69abcd8324fc9789844c5d
w01230 <4585006@gmail.com>
2018-06-10 21:18:59 +0800

    rename sh

---
+++ xware/exec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+while true; 
+do
+    pid=`ps -ef|grep ETMDaemon|grep -v grep|awk '{print $2}'`
+    if [ -z "$pid" ]; then
+	echo "restarting..."
+        portal
+    else
+        sleep 30
+    fi
+done
+
